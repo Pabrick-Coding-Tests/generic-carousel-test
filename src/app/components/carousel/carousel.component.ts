f6fd8bb07fe8fccbc131c6258264f7cdf6bfb3c6
Pabrick <pabrick.bastaky@gmail.com>
2018-10-18 12:38:36 +0200

    +ADD: full responsive carousel with animation

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CarouselService } from './carousel.service';
-import { ITPhoto } from '../carousel-photo/carousel-photo.data';
 
 @Component({
   selector: 'app-carousel',
@@ -9,27 +8,36 @@ import { ITPhoto } from '../carousel-photo/carousel-photo.data';
 })
 export class CarouselComponent implements OnInit {
 
-  public currentPhoto: ITPhoto;
+  public carouselPosition: string;
   private index: number;
+  private photo: {
+    width: number;
+    padding: number;
+  };
   constructor(private service: CarouselService) { }
 
   ngOnInit() {
+    this.photo = {
+      width: 380,
+      padding: 10
+    };
     this.index = 0;
-    this.service.getData().subscribe(
-      () => {
-        this.currentPhoto = this.service.photos[this.index];
-      }
-    );
-  }
-
-  public nextPhoto() {
-    this.index = (this.index + 1) >= this.service.limit ? 0 : (this.index + 1);
-    this.currentPhoto = this.service.photos[this.index];
+    this.carouselPosition = this.getCarouselPositionFrom(this.index);
   }
 
   public prevPhoto() {
     this.index = (this.index - 1) < 0 ? this.service.photos.length - 1 : this.index - 1;
-    this.currentPhoto = this.service.photos[this.index];
+    this.carouselPosition = this.getCarouselPositionFrom(this.index);
+  }
+
+  public nextPhoto() {
+    this.index = (this.index + 1) >= this.service.limit ? 0 : (this.index + 1);
+    this.carouselPosition = this.getCarouselPositionFrom(this.index);
+  }
+
+  private getCarouselPositionFrom(index: number): string {
+    // return this.carouselPosition = `${((index * -this.photo.width) - this.photo.padding)}px`;
+    return this.carouselPosition = `${(index * -this.photo.width - this.photo.padding)}px`;
   }
 
 }
